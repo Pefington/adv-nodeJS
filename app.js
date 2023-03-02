055886d9f1f7fdb2c356be58e49072b7b563cffe
Pierre-François Salmand <maabaa@gmail.com>
2023-03-03 06:35:45 +0700

    Add express.js

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
+/* eslint-disable import/extensions */
 import http from 'http';
-import fs from 'fs';
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write(
-      '<body> <form action="/message" method="post"> <input type="text" name="message"/> <button type="submit">Send</button>  </form> </body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'post') {
-    fs.writeFile('message.txt', 'dummy');
-    res.statusCode = 302;
-    res.setHeader('location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write(
-    '<body> <form action="/message" method="post"></form> <input type="text" name="message"/> </body>'
-  );
-  res.write('</html>');
-  return res.end();
-});
+const server = http.createServer();
 
 server.listen(3000);
